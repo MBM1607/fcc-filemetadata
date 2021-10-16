abda9ce955a55b7a74433ff0249ea16ed202f2b5
mbm_1607 <muhammadkhan1607@gmail.com>
2021-10-16 20:07:28 +0500

    Add nodemon and multer, use multer to upload files

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import multer from 'multer';
 
-var app = express();
+dotenv.config()
+
+const app = express();
+const upload = multer({ dest: 'uploads/'})
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', (req, res) => {
+	res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+	console.log(req.file);
+	res.json({
+		name: req.file.originalname,
+		type: req.file.mimetype,
+		size: req.file.size,
+	});
 });
 
 
-
-
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+
+app.listen(port, () => {
+	console.log('Your app is listening on port ' + port)
 });
